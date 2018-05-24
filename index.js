a9b680b7dd9990d2547c79089717f65b70339547
astur <astur@yandex.ru>
2018-05-24 20:29:12 +0300

    Fix #1: errors instead -1

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const load = require('cheerio').load;
 
 module.exports = async name => {
     const response = await scra(`https://www.npmjs.com/package/${name}`);
-    if(response.statusCode !== 200) return -1;
+    const err = () => new Error(`Unable to determine dependents for package: "${name}"`);
+    if(response.statusCode !== 200) throw err();
     const $ = load(response.body);
     const depLink = $('a[href="?activeTab=dependents"]');
-    if(depLink.length !== 1) return -1;
+    if(depLink.length !== 1) throw err();
     return +depLink.text().replace(/[^0-9]/g, '');
 };
--- test.js
+++ test.js
@@ -2,9 +2,13 @@ const test = require('ava');
 const type = require('easytype');
 const m = require('.');
 
-test('check-npm-dependents', async t => {
+test('main', async t => {
     const n = await m('ava');
     t.true(type.isNumber.finite(n));
     t.true(n >= 0);
-    t.is(await m('_ABC'), -1);
+});
+
+test('errors', async t => {
+    const e = await t.throws(m('_ABC'));
+    t.is(e.message, 'Unable to determine dependents for package: "_ABC"');
 });
