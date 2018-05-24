09b7e81e4f3379be95fd60d67415fe2f50f7a975
astur <astur@yandex.ru>
2018-05-24 18:48:28 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = () => {};
+const scra = require('scra');
+const load = require('cheerio').load;
+
+module.exports = async name => {
+    const response = await scra(`https://www.npmjs.com/package/${name}`);
+    if(response.statusCode !== 200) return -1;
+    const $ = load(response.body);
+    const depLink = $('a[href="?activeTab=dependents"]');
+    if(depLink.length !== 1) return -1;
+    return +depLink.text().replace(/[^0-9]/g, '');
+};
